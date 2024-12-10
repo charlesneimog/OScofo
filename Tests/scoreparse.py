45d6d5e0952ffbf63b05a8ae073e06761fdfe482
Charles K. Neimog <charlesneimog@outlook.com>
2024-12-10 20:25:08 -0300

    updates tests

--- Tests/scoreparse.py
+++ Tests/scoreparse.py
@@ -10,7 +10,10 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 def ordinary_test(test_root, sr, fft_size, hop_size):
     score_file = f"{test_root}/score.txt"
     OpenScofo = OScofo(sr, fft_size, hop_size)
-    OpenScofo.ParseScore(score_file)
+    if OpenScofo.ParseScore(score_file):
+        state = OpenScofo.GetStates()[0]
+        print(dir(state))
+        print(state.Freqs)
 
 
 ordinary_test("./", 48000, 4096, 512)
