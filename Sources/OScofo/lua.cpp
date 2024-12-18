8e5fb7b975963e086dd6527710f659e01683e591
Charles K. Neimog <charlesneimog@outlook.com>
2024-12-18 17:17:27 -0300

    function to get the OScofo pointer from lua module

--- Sources/OScofo/lua.cpp
+++ Sources/OScofo/lua.cpp
@@ -4,42 +4,62 @@
 
 namespace OScofo {
 
-static int oscofo_getPitchProb(lua_State *L) {
+static OScofo *getOScofo(lua_State *L) {
     lua_getglobal(L, "_OScofo");
     lua_getfield(L, -1, "pointer");
     OScofo *oscofo = (OScofo *)lua_touserdata(L, -1);
     if (!oscofo) {
+        return nullptr;
+    }
+    return oscofo;
+}
+
+// ─────────────────────────────────────
+static int oscofo_getPitchProb(lua_State *L) {
+    OScofo *OScofo = getOScofo(L);
+    if (!OScofo) {
         return luaL_error(L, "Invalid OScofo instance");
     }
-    double frequency = luaL_checknumber(L, 1);
-    double prob = oscofo->GetPitchProb(frequency);
-    lua_pushnumber(L, prob);
+    double Freq = luaL_checknumber(L, 1);
+    double Prob = OScofo->GetPitchProb(Freq);
+    lua_pushnumber(L, Prob);
     return 1;
 }
 
 // ─────────────────────────────────────
 static int oscofo_getBPM(lua_State *L) {
-    lua_getglobal(L, "_OScofo");
-    lua_getfield(L, -1, "pointer");
-    OScofo *oscofo = (OScofo *)lua_touserdata(L, -1);
-    if (!oscofo) {
+    OScofo *OScofo = getOScofo(L);
+    if (!OScofo) {
         return luaL_error(L, "Invalid OScofo instance");
     }
-    double bpm = oscofo->GetLiveBPM();
+    double bpm = OScofo->GetLiveBPM();
     lua_pushnumber(L, bpm);
     return 1;
 }
 
 // ─────────────────────────────────────
 static int oscofo_getKappa(lua_State *L) {
-    lua_getglobal(L, "_OScofo");
-    lua_getfield(L, -1, "pointer");
-    OScofo *oscofo = (OScofo *)lua_touserdata(L, -1);
-    if (!oscofo) {
+    OScofo *OScofo = getOScofo(L);
+    if (!OScofo) {
         return luaL_error(L, "Invalid OScofo instance");
     }
-    double kappa = oscofo->GetKappa();
-    lua_pushnumber(L, kappa);
+    double Kappa = OScofo->GetKappa();
+    lua_pushnumber(L, Kappa);
+    return 1;
+}
+
+// ─────────────────────────────────────
+static int oscofo_getSpectrum(lua_State *L) {
+    OScofo *OScofo = getOScofo(L);
+    if (!OScofo) {
+        return luaL_error(L, "Invalid OScofo instance");
+    }
+    std::vector<double> SpectralPower = OScofo->GetSpectrumPower();
+    lua_newtable(L);
+    for (size_t i = 0; i < SpectralPower.size(); ++i) {
+        lua_pushnumber(L, SpectralPower[i]);
+        lua_rawseti(L, -2, i + 1);
+    }
     return 1;
 }
 
@@ -51,6 +71,7 @@ static const luaL_Reg oscofo_funcs[] = {
 
     // Audio Description
     {"getPitchProb", oscofo_getPitchProb},
+    {"getSpectrumPower", oscofo_getSpectrum},
 
     {NULL, NULL}
     // Sentinela
