bceaab01befd8f452ffe6dfba3ab782b4167f2d5
Charles K. Neimog <charlesneimog@outlook.com>
2025-02-08 14:29:47 -0300

    just update oscofo when defined

--- Resources/tests/test.py
+++ Resources/tests/test.py
@@ -1,6 +1,7 @@
 import os
-from pydub import AudioSegment
-import numpy as np
+import librosa
+from matplotlib import pyplot as plt
+
 
 from OScofo import OScofo
 
@@ -10,26 +11,37 @@ all_music = os.listdir(this_dir)
 
 for piece_file in all_music:
     if piece_file.endswith('.mp3'):
-        event = 0
-        print('Processing:', piece_file)
-        FFT_SIZE = 2048
-        HOP_SIZE = 512 
-        audio = AudioSegment.from_mp3(piece_file)
-        audio_mono = audio.set_channels(1)
-        samples = np.array(audio_mono.get_array_of_samples(), dtype=np.float64)
-        samples /= 32768.0  
-        samples *= 4
+        event = None  
+        FFT_SIZE = 4096
+        HOP_SIZE = 1024 
+        y, sr = librosa.load(piece_file, sr=None)
         score_file = piece_file.replace('.mp3', '.txt')
-        follower = OScofo(48000, FFT_SIZE, HOP_SIZE)
+        follower = OScofo(sr, FFT_SIZE, HOP_SIZE)
         ok = follower.ParseScore(score_file)
         if not ok:
             print('Error parsing score file')
             continue
 
-        event = None  
-        for i in range(0, len(samples) - FFT_SIZE, HOP_SIZE):  
-            frame = samples[i:i + FFT_SIZE]  
+        detected_events = {}
+        for i in range(0, len(y) - FFT_SIZE, HOP_SIZE):  
+            frame = y[i:i + FFT_SIZE]  
             ok = follower.ProcessBlock(frame)  
             if follower.GetEventIndex() != event:
                 event = follower.GetEventIndex()
-                print('Event:', event)
+                detected_events[event] = i / sr
+
+        
+        print('Detected events:', detected_events)
+        plt.figure(figsize=(15, 5))
+        plt.plot(y)
+
+        for event, time in detected_events.items():
+            plt.axvline(time * sr, color='r', linestyle='--')
+
+        plt.show()
+
+
+
+
+
+
